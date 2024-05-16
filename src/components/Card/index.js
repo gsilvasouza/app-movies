8a89c678910ca98e52b8f137297776cb490d1bd1
Gsilvasouza <Gsilvasouza2000@gmail.com>
2024-05-16 16:27:17 -0300

    feat: create page favorite and add icon favorite into Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css"
+import iconFavorite from './favorite.png'
+import iconUnFavorite from './unfavorite.png'
 
 function Card({id}){
   return(
     <section className={styles.card}>
       <Link to = {`/watch/${id}`}>
-        <img src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} alt="Capa"/>
+        <img 
+          src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} 
+          alt="Capa"
+          className={styles.capa}
+        />
       </Link>
+      <figure className={styles.icon}>
+        <img src={iconFavorite} alt="icone"/>
+      </figure>
     </section>
   )
 }
